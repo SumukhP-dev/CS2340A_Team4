74e32af3669718a34f3ac6a701361e02716eb859
SumukhP-dev <156015540+SumukhP-dev@users.noreply.github.com>
2024-05-29 13:05:24 -0400

    Create intents for the create flashcard and start quiz buttons

--- Sprint0.5-main/app/src/main/java/com/example/androidstudioassignmentsu2024/MainMenuActivity.java
+++ Sprint0.5-main/app/src/main/java/com/example/androidstudioassignmentsu2024/MainMenuActivity.java
@@ -19,22 +19,6 @@ public class MainMenuActivity extends AppCompatActivity {
         // TODO 1: Program the btnCreateFlashcard and btnStartQuiz to trigger intents
         //  Intent should switch the activity on screen to CreateFlashcardActivity when btnCreateFlashcard is clicked
         //  Intent should switch the activity on screen to QuizActivity when btnStartQuiz is clicked
-        btnCreateFlashcard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // An intent to transition to CreateFlashcardActivity
-                Intent intent = new Intent(MainMenuActivity.this, CreateFlashcardActivity.class);
-                startActivity(intent);
-            }
-        });
-        btnStartQuiz.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // An intent to transition to QuizActivity
-                Intent intent = new Intent(MainMenuActivity.this, QuizActivity.class);
-                startActivity(intent);
-                //jj
-            }
-        });
+
     }
 }
