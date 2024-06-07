22fbb25cc11ec0f832893ee5dd1aea99c98db0ed
SumukhP-dev <156015540+SumukhP-dev@users.noreply.github.com>
2024-06-07 19:23:24 -0400

    Remove unnecessary files

--- HealthTracker/app/src/main/java/com/example/healthtracker/view/MainActivity.java
+++ HealthTracker/app/src/main/java/com/example/healthtracker/view/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.healthtracker.view;
 
 import android.os.Bundle;
 
+import com.example.healthtracker.R;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -33,10 +34,6 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -67,12 +64,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
