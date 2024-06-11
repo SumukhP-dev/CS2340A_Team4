2e1d74fce9ad2c455cc3dbb51c75f2ff4ec917f8
SumukhP-dev <156015540+SumukhP-dev@users.noreply.github.com>
2024-06-11 15:37:19 -0400

    Initialize splash screen on startup

--- HealthTracker/app/src/main/java/com/example/healthtracker/view/SplashActivity.java
+++ HealthTracker/app/src/main/java/com/example/healthtracker/view/SplashActivity.java
@@ -13,5 +13,21 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread() {
+
+            @Override
+            public void run() {
+
+                try{
+                    sleep(3000);
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }finally {
+                    startActivity((new Intent(SplashActivity.this, MainActivity.class)));
+                }
+            }
+        };
+        thread.start();
     }
 }
