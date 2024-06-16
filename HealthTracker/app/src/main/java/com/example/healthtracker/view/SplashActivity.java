e9a695b3bf4d813125dab3da4a91246865c08100
SumukhP-dev <156015540+SumukhP-dev@users.noreply.github.com>
2024-06-16 19:50:05 -0400

    Resolve merge conflict with AndroidManifest.xml

--- HealthTracker/app/src/main/java/com/example/healthtracker/view/SplashActivity.java
+++ HealthTracker/app/src/main/java/com/example/healthtracker/view/SplashActivity.java
@@ -23,7 +23,7 @@ public class SplashActivity extends AppCompatActivity {
                 } catch (Exception e) {
                     e.printStackTrace();
                 } finally {
-                    startActivity((new Intent(SplashActivity.this, MainActivity.class)));
+                    startActivity((new Intent(SplashActivity.this, LoginActivity.class)));
                 }
             }
         };
