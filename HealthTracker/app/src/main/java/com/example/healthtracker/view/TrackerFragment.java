6b2a60c8fe4caded747768d7e75e0c848aa4842f
SumukhP-dev <156015540+SumukhP-dev@users.noreply.github.com>
2024-06-16 20:13:33 -0400

    Add missing import to TrackerFragment

--- HealthTracker/app/src/main/java/com/example/healthtracker/view/TrackerFragment.java
+++ HealthTracker/app/src/main/java/com/example/healthtracker/view/TrackerFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.healthtracker.R;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link TrackerFragment#newInstance} factory method to
