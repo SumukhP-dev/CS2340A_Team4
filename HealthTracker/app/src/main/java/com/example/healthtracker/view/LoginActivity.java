361491cabdc52aaf15f27288a15e3be8c922fd53
ashleyvaia <ashley.vaia@gmail.com>
2024-06-16 20:36:53 -0400

    Apply checkstyle rules

--- HealthTracker/app/src/main/java/com/example/healthtracker/view/LoginActivity.java
+++ HealthTracker/app/src/main/java/com/example/healthtracker/view/LoginActivity.java
@@ -14,7 +14,7 @@ import com.example.healthtracker.ViewModel.LoginViewModel;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseUser;
+
 
 public class LoginActivity extends AppCompatActivity {
     private LoginViewModel loginViewModel;
@@ -66,21 +66,23 @@ public class LoginActivity extends AppCompatActivity {
                                 loginViewModel.getGeneralErrorMessage().getValue());
                     }
                 } else {
-                    resultAuthState.addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                startActivity(intent);
-                            } else {
-                                Toast.makeText(LoginActivity.this,
-                                        "Invalid username/password",
-                                        Toast.LENGTH_SHORT).show();
+                    resultAuthState.addOnCompleteListener(LoginActivity.this,
+                            new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Intent intent = new Intent(LoginActivity.this,
+                                            MainActivity.class);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(LoginActivity.this,
+                                            "Invalid username/password",
+                                            Toast.LENGTH_SHORT).show();
+                                }
                             }
-                        }
-                    });
+                        });
+                    }
                 }
-            }
         });
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
