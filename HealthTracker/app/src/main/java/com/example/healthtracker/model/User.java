9d1c86819f764582628b8c066cb3224a16a7c3f8
SumukhP-dev <156015540+SumukhP-dev@users.noreply.github.com>
2024-06-27 14:43:17 -0400

    Modify User class to follow singleton pattern

--- HealthTracker/app/src/main/java/com/example/healthtracker/model/User.java
+++ HealthTracker/app/src/main/java/com/example/healthtracker/model/User.java
@@ -1,12 +1,30 @@
 package com.example.healthtracker.model;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class User {
     private FirebaseAuth mAuth;
+    private FirebaseFirestore database;
+    private String username;
 
-    public User(FirebaseAuth mAuth) {
-        this.mAuth = mAuth;
+    private volatile static User uniqueInstance;
+
+    private User() {
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseFirestore.getInstance();
+        username = "";
+    }
+
+    public static User getInstance() {
+        if (uniqueInstance == null) {
+            synchronized (User.class) {
+                if (uniqueInstance == null) {
+                    uniqueInstance = new User();
+                }
+            }
+        }
+        return uniqueInstance;
     }
 
     public FirebaseAuth getAuth() {
@@ -15,5 +33,21 @@ public class User {
 
     public void setAuth(FirebaseAuth mAuth) {
         this.mAuth = mAuth;
+    }
+
+    public FirebaseFirestore getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(FirebaseFirestore database) {
+        this.database = database;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
