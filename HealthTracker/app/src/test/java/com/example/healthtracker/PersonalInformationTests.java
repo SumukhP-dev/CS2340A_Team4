5a41d8b2bb9e342bc855961c7bd325516aa4a7eb
MirandaJDom <miranda.j.dominguez@gmail.com>
2024-06-30 20:42:46 -0400

    set up basic function for mocking, commented out because not yet fully functional

--- HealthTracker/app/src/test/java/com/example/healthtracker/PersonalInformationTests.java
+++ HealthTracker/app/src/test/java/com/example/healthtracker/PersonalInformationTests.java
@@ -2,18 +2,28 @@ package com.example.healthtracker;
 
 import org.junit.Before;
 import org.junit.Test;
+//import org.junit.runner.RunWith
+//import org.mockito.Mock;
+//import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
 
 import com.example.healthtracker.ViewModel.LoginViewModel;
 import com.example.healthtracker.ViewModel.PersonalInformationViewModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.errorprone.annotations.DoNotMock;
 
+//@RunWith(MockitoJUnitRunner.class)
 public class PersonalInformationTests {
+    //@DoNotMock
     private PersonalInformationViewModel pivm;
+    //@DoNotMock
+    private LoginViewModel loginViewModel;
 
     @Before
     public void setup() {
-        LoginViewModel loginViewModel =new LoginViewModel();
+        loginViewModel =new LoginViewModel();
         //using useraccount miranda w/ password CS2340 to test
         loginViewModel.login("miranda", "CS2340");
         pivm = new PersonalInformationViewModel();
