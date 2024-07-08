89823589ccb1859b2eb15a9eb9d709d28298833b
Boyu-Cheng <boyucheng93@gmail.com>
2024-07-07 21:06:05 -0400

    update the calories fragment to get the correct data workout calories burn.

---
+++ HealthTracker/app/src/main/java/com/example/healthtracker/ViewModel/CaloriesViewModel.java
@@ -0,0 +1,236 @@
+package com.example.healthtracker.ViewModel;
+
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.healthtracker.model.User;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Random;
+
+public class CaloriesViewModel extends ViewModel {
+    private User user;
+
+    private DatabaseReference databaseRef;
+
+    private String curCalories;
+
+    public CaloriesViewModel() {
+        user = User.getInstance();
+    }
+
+    public void CaloriesBurned(String username,TextView calorieBurned){
+        databaseRef = FirebaseDatabase.getInstance().getReference();
+
+        databaseRef.child("Workouts").child(username)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        Double totalCaloriesBurned = 0.0;
+                        String stringCaloriesBurned;
+                        Double doubleCaloriesBurned;
+
+                        Calendar calendar = Calendar.getInstance();
+                        int currentDay = calendar.get(Calendar.DAY_OF_MONTH);
+                        int currentMonth = calendar.get(Calendar.MONTH); // 0-indexed
+
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                            String date = String.valueOf(dataSnapshot
+                                    .child("Date").child("date").getValue());
+                            String month = String.valueOf(dataSnapshot
+                                    .child("Date").child("month").getValue());
+
+                            if (date.equals("null") || month.equals("null")) {
+                                continue;
+                            }
+
+                            int intDate = Integer.parseInt(date);
+                            int intMonth = Integer.parseInt(month);
+
+                            stringCaloriesBurned = String.valueOf(dataSnapshot
+                                    .child("caloriesBurned").getValue());
+                            doubleCaloriesBurned =
+                                    Double.parseDouble(stringCaloriesBurned);
+
+                            if (intDate == currentDay && intMonth == currentMonth) {
+                                totalCaloriesBurned += doubleCaloriesBurned;
+                            }
+                        }
+                        curCalories = String.valueOf(totalCaloriesBurned);
+                        calorieBurned.setText(curCalories);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Log.e("FirebaseError", "Error retrieving data", error.toException());
+                    }
+                });
+
+    }
+    public void CaloriesGoal(String username,double[] goalCal,TextView calorieGoal){
+        databaseRef = FirebaseDatabase.getInstance().getReference();
+        databaseRef.child("User").child(username).get()
+                .addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                        DataSnapshot dataSnapshot = task.getResult();
+                        String genderInfo;
+                        String heightInfo;
+                        String weightInfo;
+                        genderInfo = String.valueOf(dataSnapshot.child("gender").getValue());
+                        heightInfo = String.valueOf(dataSnapshot.child("height").getValue());
+                        weightInfo = String.valueOf(dataSnapshot.child("weight").getValue());
+
+                        if (heightInfo.equals("null") || weightInfo.equals("null")) {
+                            Log.e("DataError", "Height or weight data is null");
+                            return;
+                        }
+
+                        Double heightDouble = Double.parseDouble(heightInfo);
+                        Double weightDouble = Double.parseDouble(weightInfo);
+
+                        if ("male".equals(genderInfo)) {
+                            goalCal[0] =goalMen(weightDouble, heightDouble, 30);
+                            //goalCal[0] = goalMen(weightDouble, heightDouble, 30);
+                        } else if ("female".equals(genderInfo)) {
+                            goalCal[0]=goalWomen(weightDouble, heightDouble, 50);
+                            //goalCal[0] = goalWomen(weightDouble, heightDouble, 50);
+                        } else {
+                            goalCal[0] =goalMen(weightDouble, heightDouble, 30);
+                            //goalCal[0] = goalMen(weightDouble, heightDouble, 30);
+                        }
+                        calorieGoal.setText(Double.toString(goalCal[0]));
+                    }
+                });
+
+
+    }
+
+    public void draw(String username,Button pieButton,PieChart pie,double[] goalCal){
+        databaseRef = FirebaseDatabase.getInstance().getReference();
+
+        databaseRef.child("Workouts").child(username)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        Double totalCaloriesBurned = 0.0;
+                        String stringCaloriesBurned;
+                        Double doubleCaloriesBurned;
+
+                        Calendar calendar = Calendar.getInstance();
+                        int currentDay = calendar.get(Calendar.DAY_OF_MONTH);
+                        int currentMonth = calendar.get(Calendar.MONTH); // 0-indexed
+
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                            String date = String.valueOf(dataSnapshot
+                                    .child("Date").child("date").getValue());
+                            String month = String.valueOf(dataSnapshot
+                                    .child("Date").child("month").getValue());
+
+                            if (date.equals("null") || month.equals("null")) {
+                                continue;
+                            }
+
+                            int intDate = Integer.parseInt(date);
+                            int intMonth = Integer.parseInt(month);
+
+                            stringCaloriesBurned = String.valueOf(dataSnapshot
+                                    .child("caloriesBurned").getValue());
+                            doubleCaloriesBurned =
+                                    Double.parseDouble(stringCaloriesBurned);
+
+                            if (intDate == currentDay && intMonth == currentMonth) {
+                                totalCaloriesBurned += doubleCaloriesBurned;
+                            }
+                        }
+                        curCalories = String.valueOf(totalCaloriesBurned);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Log.e("FirebaseError", "Error retrieving data", error.toException());
+                    }
+                });
+
+        if (pieButton != null) {
+            pieButton.setOnClickListener((l) -> {
+                if (pie != null && curCalories != null) {
+                    drawPie(pie, curCalories, goalCal[0]);
+                    //drawPie(pie, curCalories, goalCal[0]);
+                } else {
+                    Log.e("PieChart", "Pie or curCalories is not initialized");
+                }
+            });
+        } else {
+            Log.e("PieButton", "PieButton is not initialized");
+        }
+    }
+
+
+    public double goalMen(double weight, double height, double age) {
+        return 10 * weight + 6.25 * height - (5 * age) + 5;
+    }
+
+    public double goalWomen(double weight, double height, double age) {
+        return 10 * weight + 6.25 * height - (5 * age) - 161;
+    }
+
+    public List<PieEntry> getPieEntries(String curCalories, double goalVal) {
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        double cal = Double.parseDouble(curCalories);
+
+        float calFloat = (float) cal;
+        float goalFloat = (float) goalVal;
+
+        if (cal != 0) {
+            entries.add(new PieEntry(calFloat, "Current burning"));
+        }
+        entries.add(new PieEntry(goalFloat, "Goal"));
+
+        return entries;
+    }
+
+    public void drawPie(PieChart pie, String curCalories, double goalVal) {
+        List<PieEntry> entries = getPieEntries(curCalories, goalVal);
+        List<Integer> colors = new ArrayList<>();
+        Random rng = new Random();
+        for (int i = 0; i < entries.size(); i++) {
+            colors.add(rng.nextInt());
+        }
+
+        PieDataSet set = new PieDataSet(entries, "Subjects");
+        set.setColors(colors);
+
+        pie.setData(new PieData(set));
+        pie.invalidate();
+    }
+
+    public String cleanUsername(String username) {
+        String usernameHandleRemoved = username;
+        if (username.length() >= 10
+                && username.substring(username.length() - 10)
+                .equals("@gmail.com")) {
+            usernameHandleRemoved = username.substring(0,
+                    username.length() - 10);
+        }
+        return usernameHandleRemoved.replaceAll("\\p{P}", "");
+    }
+}
--- HealthTracker/app/src/test/java/com/example/healthtracker/ExampleUnitTest.java
+++ HealthTracker/app/src/test/java/com/example/healthtracker/ExampleUnitTest.java
@@ -30,38 +30,10 @@ import java.util.Map;
 public class ExampleUnitTest {
     private DatabaseReference mDatabase;
 
-    @Test
-    public void caloriesGoalMenIsCorrect() {
-        CaloriesFragment test = new CaloriesFragment();
-        double menGoal;
-        menGoal = test.goalMen(100, 170, 30);
-        assertEquals(1917.5, menGoal, 0.0001);
-    }
 
-    @Test
-    public void  caloriesGoalWomenIsCorrect() {
-        CaloriesFragment test = new CaloriesFragment();
-        double womenGoal;
-        womenGoal = test.goalWomen(100, 170, 30);
-        assertEquals(1751.5, womenGoal, 0.0001);
-    }
 
     // Tests that the entries to the pie chart are created correctly
-    @Test
-    public void testPieEntriesCreation() {
-        CaloriesFragment fragment = new CaloriesFragment();
-        String curCalories = "300";
-        double goalCalories = 500;
 
-        List<PieEntry> entries = fragment.getPieEntries(curCalories, goalCalories);
-
-        assertNotNull(entries);
-        assertEquals(2, entries.size());
-        assertEquals(300f, entries.get(0).getValue(), 0.001);
-        assertEquals("Current burning", entries.get(0).getLabel());
-        assertEquals(500f, entries.get(1).getValue(), 0.001);
-        assertEquals("Goal", entries.get(1).getLabel());
-    }
 
     @Test
     public void testStringToDoubleConversion1() {
@@ -79,95 +51,15 @@ public class ExampleUnitTest {
         assertEquals(expectedCalories, actualCalories, 0.0001);
     }
 
-    @Test
-    public void testPieEntriesCreationNegativeCalories() {
-        CaloriesFragment fragment = new CaloriesFragment();
-        String curCalories = "-300";
-        double goalCalories = 500;
 
-        // Call getPieEntries
-        List<PieEntry> entries = fragment.getPieEntries(curCalories, goalCalories);
 
-        // Verify data processing logic
-        assertNotNull(entries);
-        assertEquals(2, entries.size());
-        assertEquals(-300f, entries.get(0).getValue(), 0.001);
-        assertEquals("Current burning", entries.get(0).getLabel());
-        assertEquals(500f, entries.get(1).getValue(), 0.001);
-        assertEquals("Goal", entries.get(1).getLabel());
-    }
 
-    @Test
-    public void testGoalCalculationAndPieEntryCreation() {
-        CaloriesFragment fragment = new CaloriesFragment();
-        double weight = 100;
-        double height = 170;
-        double age = 30;
 
-        // Calculate goal for men
-        double goalMen = fragment.goalMen(weight, height, age);
 
-        // Set current calories
-        String curCalories = "300";
 
-        // Call getPieEntries
-        List<PieEntry> entries = fragment.getPieEntries(curCalories, goalMen);
 
-        // Verify data processing logic
-        assertNotNull(entries);
-        assertEquals(2, entries.size());
-        assertEquals(300f, entries.get(0).getValue(), 0.001);
-        assertEquals("Current burning", entries.get(0).getLabel());
-        assertEquals((float) goalMen, entries.get(1).getValue(), 0.001);
-        assertEquals("Goal", entries.get(1).getLabel());
-    }
 
-    @Test
-    public void testGoalCalculationWithLowValues() {
-        CaloriesFragment fragment = new CaloriesFragment();
-        double weight = 30;
-        double height = 100;
-        double age = 10;
 
-        // Calculate goal for men with low values
-        double goalMen = fragment.goalMen(weight, height, age);
-        assertTrue(goalMen > 0);
-
-        // Calculate goal for women with low values
-        double goalWomen = fragment.goalWomen(weight, height, age);
-        assertTrue(goalWomen > 0);
-    }
-
-    @Test
-    public void testCleanUsernameMethod1() {
-        CaloriesFragment fragment = new CaloriesFragment();
-        String cleanUsername =
-                fragment.cleanUsername("some@gmail.com");
-        assertEquals("some", cleanUsername);
-    }
-
-    @Test
-    public void testCleanUsernameMethod2() {
-        CaloriesFragment fragment = new CaloriesFragment();
-        String cleanUsername =
-                fragment.cleanUsername("g@gmail.com");
-        assertEquals("g", cleanUsername);
-    }
-    @Test
-    public void testCleanUsernameMethod3() {
-        CaloriesFragment fragment = new CaloriesFragment();
-        String cleanUsername =
-                fragment.cleanUsername("so.me.guy@gmail.com");
-        assertEquals("someguy", cleanUsername);
-    }
-    @Test
-    public void testCleanUsernameMethod4() {
-        CaloriesFragment fragment = new CaloriesFragment();
-        String cleanUsername =
-                fragment.cleanUsername(
-                        "someguysemailcreatednow@gmail.com");
-        assertEquals("someguysemailcreatednow", cleanUsername);
-    }
     public void trackerDatabaseUpdate() {
         TrackerFragment test = new TrackerFragment();
         String workoutName;
