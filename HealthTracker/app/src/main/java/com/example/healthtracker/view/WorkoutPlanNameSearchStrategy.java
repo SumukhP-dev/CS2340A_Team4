1ad91f72bd86330baaa44aa8ce35dd499ea290d8
SumukhP-dev <156015540+SumukhP-dev@users.noreply.github.com>
2024-07-14 11:07:25 -0400

    Fix bug in name and author strategies when finding a substring of the button text

--- HealthTracker/app/src/main/java/com/example/healthtracker/view/WorkoutPlanNameSearchStrategy.java
+++ HealthTracker/app/src/main/java/com/example/healthtracker/view/WorkoutPlanNameSearchStrategy.java
@@ -9,17 +9,13 @@ import java.util.ArrayList;
 public class WorkoutPlanNameSearchStrategy implements Strategy {
     @Override
     public void remove(LinearLayout container, String query, ArrayList<Button> listOfButtons) {
-            container.removeAllViews();
-            for (Button button: listOfButtons) {
-                container.addView(button);
-            }
-            for (Button button: listOfButtons) {
+        container.removeAllViews();
+        for (Button button: listOfButtons) {
+            container.addView(button);
+        }
+        for (Button button: listOfButtons) {
             String buttonText = button.getText().toString();
-            Log.d("test1", buttonText);
-            String text = buttonText.substring(0, query.length());
-            Log.d("test2", text);
-            Log.d("test3", String.valueOf(text.equals(query)));
-            if ((!(text.equals(query))) && (!query.contains("author:"))) {
+            if ((!(buttonText.startsWith(query))) && (!query.contains("author:"))) {
                 container.removeView(button);
             }
         }
