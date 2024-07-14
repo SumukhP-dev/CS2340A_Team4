c83970dbac859fc17bc6e6281c35bae7e0e34c54
MirandaJDom <miranda.j.dominguez@gmail.com>
2024-07-14 14:18:03 -0400

    Added MockDatabase and mocked the Signup view model, as well as started mocking personal info view model

---
+++ HealthTracker/app/src/test/java/com/example/healthtracker/MockDatabase.java
@@ -0,0 +1,27 @@
+package com.example.healthtracker;
+
+import java.util.ArrayList;
+
+//methods will be added on an as need basis to best be able to support functions in the Mock view models
+public class MockDatabase {
+    private ArrayList<FakeUser> users;
+
+    public MockDatabase() {
+        users = new ArrayList<FakeUser>();
+    }
+
+    public void addUser(FakeUser user) {
+        if (!users.contains(user)) {
+            users.add(user);
+        }
+    }
+
+    public FakeUser getUser(FakeUser user) {
+        if (users.contains(user)) {
+            int index = users.indexOf(user);
+            return users.get(index);
+        } else {
+            return null;
+        }
+    }
+}
