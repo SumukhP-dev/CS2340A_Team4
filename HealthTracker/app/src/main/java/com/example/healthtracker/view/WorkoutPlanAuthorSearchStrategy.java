29a6888e0113d8aa1f6e77f323e6af10a9cae52b
SumukhP-dev <156015540+SumukhP-dev@users.noreply.github.com>
2024-07-14 11:07:25 -0400

    Fix bug in name and author strategies

--- HealthTracker/app/src/main/java/com/example/healthtracker/view/WorkoutPlanAuthorSearchStrategy.java
+++ HealthTracker/app/src/main/java/com/example/healthtracker/view/WorkoutPlanAuthorSearchStrategy.java
@@ -13,17 +13,12 @@ public class WorkoutPlanAuthorSearchStrategy implements Strategy {
             String buttonText = button.getText().toString();
             int indexOfText = buttonText.indexOf("\t");
             String text = buttonText.substring(indexOfText + 1);
-            Log.d("test2_1", text);
 
             int indexOfQuery = query.indexOf("author:");
-            Log.d("test2_2", String.valueOf(indexOfQuery));
 
             if (indexOfQuery != -1) {
                 String authorNameQuery = query.substring(indexOfQuery + 7).trim();
-                Log.d("test2_3", authorNameQuery);
-                Log.d("test2_4", String.valueOf(text.substring(0, authorNameQuery.length()).equals(authorNameQuery)));
-
-                if (!text.substring(0, authorNameQuery.length()).equals(authorNameQuery)) {
+                if (!text.startsWith(authorNameQuery)) {
                     container.removeView(button);
                 }
             }
