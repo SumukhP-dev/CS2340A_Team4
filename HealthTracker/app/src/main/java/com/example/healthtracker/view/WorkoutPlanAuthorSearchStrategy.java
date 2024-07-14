be42d6f54aa252bcaceef0abbab85222eb71b6af
SumukhP-dev <156015540+SumukhP-dev@users.noreply.github.com>
2024-07-14 05:05:02 -0400

    Implement search bar for name and author

--- HealthTracker/app/src/main/java/com/example/healthtracker/view/WorkoutPlanAuthorSearchStrategy.java
+++ HealthTracker/app/src/main/java/com/example/healthtracker/view/WorkoutPlanAuthorSearchStrategy.java
@@ -1,5 +1,6 @@
 package com.example.healthtracker.view;
 
+import android.util.Log;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -10,14 +11,21 @@ public class WorkoutPlanAuthorSearchStrategy implements Strategy {
     public void remove(LinearLayout container, String query, ArrayList<Button> listOfButtons) {
         for (Button button: listOfButtons) {
             String buttonText = button.getText().toString();
-            int indexOfText = buttonText.indexOf("/t");
-            String text = buttonText.substring(indexOfText + 2);
+            int indexOfText = buttonText.indexOf("\t");
+            String text = buttonText.substring(indexOfText + 1);
+            Log.d("test2_1", text);
 
             int indexOfQuery = query.indexOf("author:");
-            String authorNameQuery = query.substring(indexOfQuery + 7).trim();
+            Log.d("test2_2", String.valueOf(indexOfQuery));
 
-            if (!text.equals(authorNameQuery)) {
-                container.removeView(button);
+            if (indexOfQuery != -1) {
+                String authorNameQuery = query.substring(indexOfQuery + 7).trim();
+                Log.d("test2_3", authorNameQuery);
+                Log.d("test2_4", String.valueOf(text.substring(0, authorNameQuery.length()).equals(authorNameQuery)));
+
+                if (!text.substring(0, authorNameQuery.length()).equals(authorNameQuery)) {
+                    container.removeView(button);
+                }
             }
         }
     }
