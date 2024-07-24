63328c2d126fad30377aadd5af81705f6e192120
SumukhP-dev <156015540+SumukhP-dev@users.noreply.github.com>
2024-07-24 18:02:30 -0400

    Fix minor checkstyle errors

--- HealthTracker/app/src/main/java/com/example/healthtracker/view/TrackerFragment.java
+++ HealthTracker/app/src/main/java/com/example/healthtracker/view/TrackerFragment.java
@@ -11,9 +11,6 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
@@ -25,7 +22,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -120,7 +117,7 @@ public class TrackerFragment extends Fragment {
                         spinnerCount = Integer.parseInt(workoutNum);
                         Log.d("counter inside class:", String.valueOf(spinnerCount));
                         for (int i = 0; i < spinnerCount && i < 5; i++) {
-                            int counter = spinnerCount -i;
+                            int counter = spinnerCount - i;
                             mDatabase.child("Workouts").child(username)
                                     .child("workout "
                                             + String.valueOf(counter))
@@ -137,7 +134,9 @@ public class TrackerFragment extends Fragment {
                                                                     .getValue());
                                                     // Forgot to add this comment :)
                                                     // Adding Date
-                                                    String date = String.valueOf(dataSnap.child("Date").getValue());
+                                                    String date = String
+                                                            .valueOf(dataSnap
+                                                                    .child("Date").getValue());
                                                     TextView textView = new TextView(getContext());
                                                     textView.setLayoutParams(
                                                             new LinearLayout.LayoutParams(
@@ -147,7 +146,8 @@ public class TrackerFragment extends Fragment {
                                                             16, 16);
 
                                                     String displayText = String
-                                                            .format("%s    Workout: %s", date, workoutNa);
+                                                            .format("%s    Workout: %s",
+                                                                    date, workoutNa);
                                                     textView.setText(displayText);
 
                                                     // Add the TextView to the container
