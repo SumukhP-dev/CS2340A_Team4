dcfd023ba161231a84179304a458d77b74cb93ab
MirandaJDom <miranda.j.dominguez@gmail.com>
2024-07-23 20:30:07 -0400

    Started mocking for Community challenge view model

---
+++ HealthTracker/app/src/test/java/com/example/healthtracker/FakeCommunityChallenge.java
@@ -0,0 +1,9 @@
+package com.example.healthtracker;
+import java.util.ArrayList;
+
+public class FakeCommunityChallenge {
+    private String deadline;
+    private String description;
+    private String name;
+    private ArrayList<String> participants;
+}
